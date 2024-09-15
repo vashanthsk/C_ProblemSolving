37395fc478c3376dd63cd5d75fcc302fe1d54892
vashanthsk <vashanth0609@gmail.com>
2024-09-15 23:44:38 +0530

    Add files via upload

---
+++ MAXNUM.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,rem,max=0;
+clrscr();
+printf("Enter a number:");
+scanf("%d",&num);
+while(num!=0)
+{
+rem=num%10;
+if(rem>max) max=rem;
+num=num/10;
+}
+printf("%d",max);
+getch();
+}
