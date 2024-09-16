13aa7fd589649f93cb314a660b1ebc04909eee5a
vashanthsk <vashanth0609@gmail.com>
2024-09-16 13:57:20 +0530

    Add files via upload

---
+++ DIFFEREN.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,max=0,min,num;
+clrscr();
+printf("Enter the number(2 or 3):");
+scanf("%d",&n);
+printf("Enter a number1:");
+scanf("%d",&num);
+max=num;
+min=num;
+for(i=1;i<n;i++)
+{
+ printf("Enter number %d:",i+1);
+ scanf("%d",&j);
+ if(max<j) max=j;
+ else if(min>j) min=j;
+}
+ printf("Difference between %d and %d is: %d",max,min,max-min);
+ getch();
+}
+
