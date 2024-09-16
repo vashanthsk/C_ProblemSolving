3dd39b0f85ff4e7cceb9f69cf2f57dffc91a6263
vashanthsk <vashanth0609@gmail.com>
2024-09-16 13:23:14 +0530

    RightTrianglePattern

--- RighttrianglePattern.c
+++ RighttrianglePattern.c
@@ -8,11 +8,11 @@ printf("Enter the n value:");
 scanf("%d",&n);
 for(out=1;out<=n;out++)
 {
-for(in=1;in<=out;in++)
-{
-printf("*");
-}
-printf("\n");
+  for(in=1;in<=out;in++)
+  {
+     printf("*");
+  }
+  printf("\n");
 }
 printf("Successful");
 getch();
